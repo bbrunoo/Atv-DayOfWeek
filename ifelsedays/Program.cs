7890a8ff71900af19b516725220179f9a76d76b1
bruno.sanntos <85880400+bbrunoo@users.noreply.github.com>
2021-10-17 21:57:00 -0300

    Update Program.cs

--- ifelsedays/Program.cs
+++ ifelsedays/Program.cs
@@ -12,56 +12,56 @@ namespace ifelsedays
 
 
             WriteLine("---------------");
-            WriteLine("Domingo/Sunday = 0.");
-            WriteLine("Segunda/Monday = 1.");
-            WriteLine("Terça/Tuesday = 2.");
-            WriteLine("Quarta/Wednesday = 3.");
-            WriteLine("Quinta/Wednesday = 4.");
-            WriteLine("Sexta/Friday = 5.");
-            WriteLine("Sabado/Saturday = 6.");
+            WriteLine("Domingo/Sunday = 1.");
+            WriteLine("Segunda/Monday = 2.");
+            WriteLine("Terça/Tuesday = 3.");
+            WriteLine("Quarta/Wednesday = 4.");
+            WriteLine("Quinta/Wednesday = 5.");
+            WriteLine("Sexta/Friday = 6.");
+            WriteLine("Sabado/Saturday = 7.");
             WriteLine("---------------");
             WriteLine("Escreva um dia da semana em numeros.");
 
             int day = Convert.ToInt32(ReadLine());
 
-            if (day == 0)
+            if (day == 1)
             {
                 WriteLine($"Today is {DayOfWeek.Sunday}");
             }
 
-            else if (day == 1)
+            else if (day == 2)
             {
                 WriteLine($"Today is {DayOfWeek.Monday}");
             }
 
-            else if (day == 2)
+            else if (day == 3)
             {
                 WriteLine($"Today is {DayOfWeek.Tuesday}");
             }
 
-            else if (day == 3)
+            else if (day == 4)
             {
                 WriteLine($"Today is {DayOfWeek.Wednesday}");
             }
 
-            else if (day == 4)
+            else if (day == 5)
             {
                 WriteLine($"Today is {DayOfWeek.Thursday}");
             }
 
-            else if (day == 5)
+            else if (day == 6)
             {
                 WriteLine($"Today is {DayOfWeek.Friday}");
             }
 
-            else if (day == 6)
+            else if (day == 7)
             {
                 WriteLine($"Today is {DayOfWeek.Saturday}");
             }
 
             else
             {
-                WriteLine("Digite um numero de 0 a 6 ");
+                WriteLine("Digite um numero de 1 a 7 ");
             }
 
         }
