1c3fd0211b4df380eea9b527d258d5bd85dccb74
bruno.sanntos <85880400+bbrunoo@users.noreply.github.com>
2021-10-20 15:03:18 -0300

    Update Program.cs

--- ifelsedays/Program.cs
+++ ifelsedays/Program.cs
@@ -9,65 +9,42 @@ namespace ifelsedays
         static void Main(string[] args)
 
         {
+            var day = DateTime.Now.DayOfWeek;
 
-
-            WriteLine("---------------");
-            WriteLine("Domingo/Sunday = 1.");
-            WriteLine("Segunda/Monday = 2.");
-            WriteLine("Terça/Tuesday = 3.");
-            WriteLine("Quarta/Wednesday = 4.");
-            WriteLine("Quinta/Wednesday = 5.");
-            WriteLine("Sexta/Friday = 6.");
-            WriteLine("Sabado/Saturday = 7.");
-            WriteLine("---------------");
-            WriteLine("Escreva um dia da semana em numeros.");
-
-            int day = Convert.ToInt32(ReadLine());
-
-            if (day == 1)
+            if (day == DayOfWeek.Sunday)
             {
                 WriteLine($"Today is {DayOfWeek.Sunday}");
             }
 
-            else if (day == 2)
+            else if (day == DayOfWeek.Monday)
             {
                 WriteLine($"Today is {DayOfWeek.Monday}");
             }
 
-            else if (day == 3)
+            else if (day == DayOfWeek.Tuesday)
             {
                 WriteLine($"Today is {DayOfWeek.Tuesday}");
             }
 
-            else if (day == 4)
+            else if (day == DayOfWeek.Wednesday)
             {
                 WriteLine($"Today is {DayOfWeek.Wednesday}");
             }
 
-            else if (day == 5)
+            else if (day == DayOfWeek.Thursday)
             {
                 WriteLine($"Today is {DayOfWeek.Thursday}");
             }
 
-            else if (day == 6)
+            else if (day == DayOfWeek.Friday)
             {
                 WriteLine($"Today is {DayOfWeek.Friday}");
             }
 
-            else if (day == 7)
+            else if (day == DayOfWeek.Saturday)
             {
                 WriteLine($"Today is {DayOfWeek.Saturday}");
             }
-
-            else
-            {
-                WriteLine("Digite um numero de 1 a 7 ");
-            }
-
         }
     }
 }
-
-
-
-
